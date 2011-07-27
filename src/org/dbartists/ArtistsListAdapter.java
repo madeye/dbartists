f53a23f0694af175e936274ef08bd057d212298d
Max Lv <max.c.lv@gmail.com>
2011-07-27 10:02:01 +0800

    fix

--- src/org/dbartists/ArtistsListAdapter.java
+++ src/org/dbartists/ArtistsListAdapter.java
@@ -126,7 +126,8 @@ public class ArtistsListAdapter extends ArrayAdapter<Artist> {
 	private void getMoreArtists(String url, int startId) {
 		List<Artist> downloadArtists = ArtistFactory.downloadArtists(url,
 				startId);
-		if (downloadArtists != null && downloadArtists.size() < 20) {
+		// Work around for the bug of douban
+		if (downloadArtists != null && downloadArtists.size() < 18) {
 			endReached = true;
 		}
 		if (downloadArtists != null)
